1bde7a99d01d40c7a1c90077b8de7fe602170813
pcailly <pcailly@pm.me>
2022-09-03 00:32:55 +0200

    ci: 🎡 amplify
    add amplify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,8 +10,9 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
-
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'./runtimeConfig': './runtimeConfig.browser'
+		}
+	}
 };
 
 export default config;
